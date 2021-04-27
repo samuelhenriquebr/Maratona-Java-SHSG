8982231e85f747f09d4410dc1a0cccf52cf12666
015001050 <015001050@vicunha.com.br>
2021-04-27 20:41:18 -0300

    Primeira aula de arrays

--- src/ControleFluxo4.java
+++ src/ControleFluxo4.java
@@ -1,16 +1,21 @@
 public class ControleFluxo4 {
     public static void main(String[] args) {
-        int contador =8;
+        int contador = 8;
         while (contador < 10) {
             System.out.println(contador++);
             //contador++;
         }
-        do{
-            System.out.printf("dentro do do while");
-        }while (contador < 10);
+        contador = 0;
+        do {
+            System.out.printf("dentro do do while"+contador++);
+        } while (contador < 10);
 
-        for (int i=0; i < 10; i++){
-            System.out.println("valor de i é: "+i);
+        for (int i = 0; i < 10; i++) {
+            System.out.println("valor de i é: " + i);
+            if(i==5){
+                break;
+            }
         }
+
     }
 }
---
+++ src/ExercicioControleFluxo3.java
@@ -0,0 +1,8 @@
+public class ExercicioControleFluxo3 {
+    public static void main(String[] args) {
+        //imprima todos os números pares de 0 até 100000
+         for(int i = 0; i <= 100000; i +=2){
+             System.out.println(i);
+         }
+    }
+}
