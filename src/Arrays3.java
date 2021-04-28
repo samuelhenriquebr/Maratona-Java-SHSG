7ae7e157ffa1da2347f6b1286d5494615bbc0256
015001050 <015001050@vicunha.com.br>
2021-04-27 21:32:05 -0300

    Ultima aula de arrays

---
+++ src/Arrays3.java
@@ -0,0 +1,16 @@
+public class Arrays3 {
+    public static void main(String[] args) {
+        int[] numeros = new int[5];
+        int[] numeros2 = {1, 2, 3, 4, 5}; //tamanho é 5, index de 0 até 4
+        int[] numeros3 = new int[]{1, 2, 3, 4, 5};
+
+        int i = 0;
+        for (i = 0; i < 5; i++) {
+            System.out.println(numeros2[i]);
+        }
+
+        for (int num : numeros2) {
+            System.out.println(num);
+        }
+    }
+}
