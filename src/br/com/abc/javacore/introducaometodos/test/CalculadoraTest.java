5f77c7a8660c0b4093cc5788c41511bf60529c18
015001050 <015001050@vicunha.com.br>
2021-05-05 21:09:53 -0300

    final de parametros

--- src/br/com/abc/javacore/introducaometodos/test/CalculadoraTest.java
+++ src/br/com/abc/javacore/introducaometodos/test/CalculadoraTest.java
@@ -18,5 +18,6 @@ public class CalculadoraTest {
         int[] numeros = new int[]{1, 2, 3, 4, 5};
         calc.somaArray(numeros);
         calc.somaVarArgs(1,2,3,4,5);
+        System.out.println("teste");
     }
 }
