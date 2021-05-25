6488584a39af9c222118a98767f7c2387c692d98
Samuel Gomes <samuelvicosa@gmail.com>
2021-05-25 15:16:30 -0300

    Aula 40
    Signed-off-by: Samuel Gomes <samuelvicosa@gmail.com>

---
+++ src/br/com/abc/javacore/sobrecargaconstrutores/test/EstudanteTest.java
@@ -0,0 +1,10 @@
+package br.com.abc.javacore.sobrecargaconstrutores.test;
+
+import br.com.abc.javacore.sobrecargaconstrutores.classes.Estudante;
+
+public class EstudanteTest {
+    public static void main(String[] args) {
+        Estudante est = new Estudante("112322", "Carlitos", new double[] {5,7,9}, "13/12/2010");
+        est.imprime();
+    }
+}
--- src/br/com/abc/javacore/sobrecargametodos/classes/Funcionario.java
+++ src/br/com/abc/javacore/sobrecargametodos/classes/Funcionario.java
@@ -12,15 +12,25 @@ public class Funcionario {
         this.salario = salario;
         this.rg = rg;
     }
+    public Funcionario(){
+
+    }
+    public void init(String nome, String cpf, double salario){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.salario = salario;
+    }
+    public void init(String nome, String cpf, double salario, String rg){
+        init(nome, cpf, salario);
+        this.salario = salario;
+    }
     public void imprime() {
         System.out.println(this.nome);
         System.out.println(this.cpf);
         System.out.println(this.salario);
         System.out.println(this.rg);
     }
-    public Funcionario(){
 
-    }
     public void setNome(String nome) {
         this.nome = nome;
     }
