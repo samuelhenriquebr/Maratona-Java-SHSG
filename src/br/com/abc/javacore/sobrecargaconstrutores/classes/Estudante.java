0dd6db8b2dc09816739aa8ae3ef557f9af14bbbb
Samuel Gomes <samuelvicosa@gmail.com>
2021-05-25 15:18:22 -0300

    Aula 40 v2
    Signed-off-by: Samuel Gomes <samuelvicosa@gmail.com>

--- src/br/com/abc/javacore/sobrecargaconstrutores/classes/Estudante.java
+++ src/br/com/abc/javacore/sobrecargaconstrutores/classes/Estudante.java
@@ -6,25 +6,38 @@ public class Estudante {
     private double[] notas;
     private String dataMatricula;
 
-    public Estudante(String matricula, String nome, double[] notas){
+    public Estudante(String matricula, String nome, double[] notas) {
         this.matricula = matricula;
         this.nome = nome;
         this.notas = notas;
     }
-    public Estudante(String matricula, String nome, double[] notas, String dataMatricula){
+
+    public Estudante(String matricula, String nome, double[] notas, String dataMatricula) {
         this(matricula, nome, notas);
         this.dataMatricula = dataMatricula;
     }
-    public Estudante(){
+
+    public Estudante() {
         System.out.println("Construtor default");
     }
-    public void imprime(){
+
+    public String getDataMatricula() {
+        return dataMatricula;
+    }
+
+    public void setDataMatricula(String dataMatricula) {
+        this.dataMatricula = dataMatricula;
+    }
+
+    public void imprime() {
         System.out.println(this.nome);
         System.out.println(this.matricula);
-        for(double nota: notas){
-            System.out.println(nota+" ");
+        for (double nota : notas) {
+            System.out.println(nota + " ");
         }
+        System.out.println(this.dataMatricula);
     }
+
     public String getMatricula() {
         return matricula;
     }
