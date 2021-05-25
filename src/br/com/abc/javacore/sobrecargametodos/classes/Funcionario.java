d41c8ea5dca4338ff63bc345f8d05ee3769bd0f9
Samuel Gomes <samuelvicosa@gmail.com>
2021-05-25 14:38:36 -0300

    Aula 39
    Signed-off-by: Samuel Gomes <samuelvicosa@gmail.com>

--- src/br/com/abc/javacore/sobrecargametodos/classes/Funcionario.java
+++ src/br/com/abc/javacore/sobrecargametodos/classes/Funcionario.java
@@ -6,20 +6,20 @@ public class Funcionario {
     private double salario;
     private String rg;
 
+    public Funcionario(String nome, String cpf, double salario, String rg){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.salario = salario;
+        this.rg = rg;
+    }
     public void imprime() {
         System.out.println(this.nome);
         System.out.println(this.cpf);
         System.out.println(this.salario);
         System.out.println(this.rg);
     }
-    public void init(String nome, String cpf, double salario){
-        this.nome = nome;
-        this.cpf = cpf;
-        this.salario = salario;
-    }
-    public void init(String nome, String cpf, double salario, String rg){
-        init(nome, cpf, salario);
-        this.rg = rg;
+    public Funcionario(){
+
     }
     public void setNome(String nome) {
         this.nome = nome;
--- src/br/com/abc/javacore/sobrecargametodos/test/FuncionariosTest.java
+++ src/br/com/abc/javacore/sobrecargametodos/test/FuncionariosTest.java
@@ -4,9 +4,9 @@ import br.com.abc.javacore.sobrecargametodos.classes.Funcionario;
 
 public class FuncionariosTest {
     public static void main(String[] args) {
-        Funcionario funcionario = new Funcionario();
-        funcionario.init("Chica da Silva","123.434.543-44", 4500, "12334234");
-
+        Funcionario funcionario = new Funcionario("Chica da Silva","123.434.543-44", 4500, "12334234");
+        Funcionario funcionario2 = new Funcionario();
         funcionario.imprime();
+        funcionario2.imprime();
     }
 }
