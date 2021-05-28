5184f43df3197ee0aeab6ab13b67650e09b8ad66
samuelhenriquebr <samuelvicosa@gmail.com>
2021-05-28 15:20:49 -0300

    Aula 51: Herança parte 2 e referência super
    Signed-off-by: samuelhenriquebr <samuelvicosa@gmail.com>

--- src/br/com/abc/javacore/Hheranca/classes/Funcionarios.java
+++ src/br/com/abc/javacore/Hheranca/classes/Funcionarios.java
@@ -3,6 +3,11 @@ package br.com.abc.javacore.Hheranca.classes;
 public class Funcionarios extends Pessoa {
     private double salario;
 
+    public void imprime() {
+        super.imprime();
+        System.out.println("Salário: " + this.salario);
+    }
+
     public double getSalario() {
         return salario;
     }
