3535f89449c5ca3c9dbdbae1d95dd52b621cd4c5
samuelhenriquebr <samuelvicosa@gmail.com>
2021-06-04 20:10:09 -0300

    Aula 64: Polimorfismo pt 01
    Signed-off-by: samuelhenriquebr <samuelvicosa@gmail.com>

--- src/br/com/abc/javacore/Minterfaces/classes/Carro.java
+++ src/br/com/abc/javacore/Minterfaces/classes/Carro.java
@@ -1,6 +1,7 @@
 package br.com.abc.javacore.Minterfaces.classes;
 
 public class Carro implements Tributavel {
+    //private > default > protected > public
     @Override
     public void calculaImposto() {
 
--- src/br/com/abc/javacore/Minterfaces/classes/Tributavel.java
+++ src/br/com/abc/javacore/Minterfaces/classes/Tributavel.java
@@ -2,5 +2,5 @@ package br.com.abc.javacore.Minterfaces.classes;
 
 public interface Tributavel {
     public static final double IMPOSTO = 0.2;
-    public abstract void calculaImposto();
+    void calculaImposto();
 }
