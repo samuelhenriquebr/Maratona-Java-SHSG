880e5e9eaf75b55f2cdc956bb58f7912c1322b32
samuelhenriquebr <samuelvicosa@gmail.com>
2021-06-05 16:41:37 -0300

    Aula 67
    Signed-off-by: samuelhenriquebr <samuelvicosa@gmail.com>

---
+++ src/br/com/abc/javacore/Oexception/test/ErroTest.java
@@ -0,0 +1,10 @@
+package br.com.abc.javacore.Oexception.test;
+
+public class ErroTest {
+    public static void main(String[] args) {
+        stackOverFlowError();
+    }
+    public static void stackOverFlowError(){
+        stackOverFlowError();
+    }
+}
