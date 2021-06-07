1d35f570cb93fd4d51e5ad72cb816b7fe5fcd056
samuelhenriquebr <samuelvicosa@gmail.com>
2021-06-07 20:44:01 -0300

    Aula 71: Exceptions: bloco finally pt 05
    Signed-off-by: samuelhenriquebr <samuelvicosa@gmail.com>

--- src/br/com/abc/javacore/Oexception/checkedexception/test/CheckedExceptionTest.java
+++ src/br/com/abc/javacore/Oexception/checkedexception/test/CheckedExceptionTest.java
@@ -5,20 +5,35 @@ import java.io.IOException;
 
 public class CheckedExceptionTest {
     public static void main(String[] args) {
-        try {
-            criarArquivo();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        System.out.println(abrirArquivo());
+
     }
+
     public static void criarArquivo() throws IOException {
         File file = new File("Teste.txt");
-        try{
-            System.out.println("Arquivo criado? : "+file.createNewFile());
+        try {
+            System.out.println("Arquivo criado? : " + file.createNewFile());
             System.out.println("Arquivo criado");
-        }catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
             throw e;
         }
+    }
+
+    public static String abrirArquivo() {
+        File file = new File("Teste.txt");
+        try {
+            System.out.println("Abrindo um arquivo");
+            System.out.println("Executando a leitura do arquivo");
+            //throw new Exception();
+            System.out.println("Escrevendo no arquivo");
+            return "Valor";
+        } catch (Exception e) {
+            System.out.println("Dentro do Catch");
+            e.printStackTrace();
+        } finally {
+            System.out.println("Fechar o arquivo");
+        }
+        return null;
     }
 }
