98ec2bd3bf428a2d7bf72296de8c95e2babdd803
samuelhenriquebr <samuelvicosa@gmail.com>
2021-06-07 20:31:58 -0300

    Aula 70: Exceptions: lançamento de exceção pt 04
    Signed-off-by: samuelhenriquebr <samuelvicosa@gmail.com>

--- src/br/com/abc/javacore/Oexception/checkedexception/test/CheckedExceptionTest.java
+++ src/br/com/abc/javacore/Oexception/checkedexception/test/CheckedExceptionTest.java
@@ -5,15 +5,20 @@ import java.io.IOException;
 
 public class CheckedExceptionTest {
     public static void main(String[] args) {
-        criarArquivo();
+        try {
+            criarArquivo();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-    public static void criarArquivo(){
+    public static void criarArquivo() throws IOException {
         File file = new File("Teste.txt");
         try{
             System.out.println("Arquivo criado? : "+file.createNewFile());
             System.out.println("Arquivo criado");
         }catch(IOException e){
             e.printStackTrace();
+            throw e;
         }
     }
 }
--- src/br/com/abc/javacore/Oexception/runtimeexception/RuntimeExceptionTest.java
+++ src/br/com/abc/javacore/Oexception/runtimeexception/RuntimeExceptionTest.java
@@ -2,20 +2,18 @@ package br.com.abc.javacore.Oexception.runtimeexception;
 
 public class RuntimeExceptionTest {
     public static void main(String[] args) {
-        Object o = null;
-//        int a =10;
-//        int b = 0;
-//        if( b!= 0){
-//            int c = a / b;
-//            System.out.println(a);
-//        }
         try{
-            int[] a = new int[2];
-            System.out.println(a[2]);
-            System.out.println("Imprimeindo depois da possível exceção");
-        }catch(ArrayIndexOutOfBoundsException e){
+            divisao(10,0);
+        }catch(RuntimeException e){
+            System.out.println(e.getMessage());
             e.printStackTrace();
         }
-        System.out.println("Fora do bloco try/catch");
+    }
+    private static void divisao(int num1, int num2){
+        if(num2 == 0){
+            throw new IllegalArgumentException("Passe um valor diferente de 0 para num2");
+        }
+        int result = num1/num2;
+        System.out.println(result);
     }
 }
