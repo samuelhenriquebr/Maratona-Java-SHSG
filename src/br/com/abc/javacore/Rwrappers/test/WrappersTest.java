e9feb216d159331cf668600300df2cecffada4f6
samuelhenriquebr <samuelvicosa@gmail.com>
2021-06-08 16:33:44 -0300

    Aula 78: Classes Wrapper pt 01
    Signed-off-by: samuelhenriquebr <samuelvicosa@gmail.com>

---
+++ src/br/com/abc/javacore/Rwrappers/test/WrappersTest.java
@@ -0,0 +1,23 @@
+package br.com.abc.javacore.Rwrappers.test;
+
+public class WrappersTest {
+    public static void main(String[] args) {
+        byte bytePrimitivo = 1;
+        short shortPrimitivo = 1;
+        int intPrimitivo = 10;
+        long longPrimitivo = 10;
+        float floatPrimitivo = 10;
+        double doublePrimitivo = 10;
+        char charPrimitivo = 'A';
+        boolean booleanPrimitivo = true;
+
+        Byte byteWrapper =1;
+        Short shortWrapper = 1;
+        Integer integerWrapper = 1;
+        Long longWrapper = 10L;
+        Float floatWrapper = 10F;
+        Double doubleWrapper = 10D;
+        Character characterWrapper = 'A';
+        Boolean booleanWrapper = true;
+    }
+}
