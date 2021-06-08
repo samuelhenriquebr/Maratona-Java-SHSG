cde229d019b8f13c958f054e8010b62391fe9134
samuelhenriquebr <samuelvicosa@gmail.com>
2021-06-08 07:13:06 -0300

    Aula 73: Exceptions: Multi-catch pt 07
    Signed-off-by: samuelhenriquebr <samuelvicosa@gmail.com>

--- src/br/com/abc/javacore/Oexception/runtimeexception/MutiplasRuntimeExceptionTest.java
+++ src/br/com/abc/javacore/Oexception/runtimeexception/MutiplasRuntimeExceptionTest.java
@@ -1,5 +1,6 @@
 package br.com.abc.javacore.Oexception.runtimeexception;
 
+import java.awt.*;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.SQLException;
@@ -8,25 +9,18 @@ public class MutiplasRuntimeExceptionTest {
     public static void main(String[] args) {
         try {
             throw new IndexOutOfBoundsException();
-        } catch (ArrayIndexOutOfBoundsException e) {
+        } catch (IndexOutOfBoundsException | IllegalArgumentException | ArithmeticException e) {
             System.out.println("Dentro do ArrayIndexOutOfBoundsException");
-        } catch (IndexOutOfBoundsException e) {
-            System.out.println("Dentro do IndexOutOfBoundsException");
-        } catch (ArithmeticException e) {
-            System.out.println("Dentro do ArithmeticException");
-        }catch (RuntimeException e){
-            System.out.println("Dentro do RuntimeException");
         }
         System.out.println("final do programa");
         try {
             talvezLanceException();
-        }catch(SQLException e){
-
-        }catch (IOException e){
+        } catch (Exception e) {
 
         }
     }
-    private static void talvezLanceException() throws SQLException, FileNotFoundException {
+
+    private static void talvezLanceException() throws SQLException, AWTException, IOException {
 
     }
 }
