59cb8a3395fb4df2380cf3a5655559220c049cd3
samuelhenriquebr <samuelvicosa@gmail.com>
2021-06-10 07:38:58 -0300

    Aula 90: Expressões regulares pt 02
    Signed-off-by: samuelhenriquebr <samuelvicosa@gmail.com>

--- src/br/com/abc/javacore/Uexpressoesregulares/test/ExpressoesRegularesTest.java
+++ src/br/com/abc/javacore/Uexpressoesregulares/test/ExpressoesRegularesTest.java
@@ -5,8 +5,14 @@ import java.util.regex.Pattern;
 
 public class ExpressoesRegularesTest {
     public static void main(String[] args) {
-        String regex = "aba";
-        String texto = "abababa";
+        // \d - todos os digitos
+        // \D - tudo o que não for digito
+        // \s - espaços em branco \t \n \f \r
+        // \S - caractere que não é branco
+        // \w - caracteres de palavras a-z A-Z, digitos e _
+        // \W - tudo que não for caractere de palavra
+        String regex = "\\W";
+        String texto = "#@aba1278 \t_";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(texto);
         System.out.println("texto: "+texto);
